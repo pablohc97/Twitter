cac623e24a1f72d6eef44f2782a0c21913f20c9b
pablohc97 <67585029+pablohc97@users.noreply.github.com>
2021-05-21 18:57:02 +0200

    Add files via upload

---
+++ config_twitter.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 13 11:18:34 2021
+
+@author: pablo
+"""
+
+consumer_key = '*******'
+consumer_secret = '*******'
+access_token = '*******'
+access_secret = '*******'
